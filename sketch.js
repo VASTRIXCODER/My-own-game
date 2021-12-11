b1f68aea89a2d8fc329df63d84ba8a1de6c6e0e1
VASTRIXCODER <84544398+VASTRIXCODER@users.noreply.github.com>
2021-12-11 11:49:41 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var mcharacter
+
+function preload() {
+
+}
+
+
+function setup() {
+
+}
+
+
+
+function draw() {
+createCanvas(800,800);
+mcharacter = createSprite(100,100,20,20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    drawSprites();
+}
